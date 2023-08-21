f6e684d8b06bce74974e8d7502afa4736e683654
noordibou <noor.dibou@hotmail.com>
2023-08-21 04:06:45 -0500

    adding new vercel link

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -22,7 +22,7 @@ interface Props {
 
 async function fetchSearchResults(searchTerm: string) {
   const { data } = await axios.get<Product[]>(
-    `http://e-shop-five-nu.vercel.app/api/products?term=${searchTerm}`
+    `https://e-shop-git-main-noordibou.vercel.app/api/products?term=${searchTerm}`
   );
   return data;
 }
