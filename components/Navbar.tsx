1e53b3faaba3ce83fbd15188edcc4c906fbf764b
noordibou <noor.dibou@hotmail.com>
2023-09-05 23:58:49 -0500

    changen name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     <div className='sticky top-0 p-3 flex shadow-navbarShadow bg-bodyColor opacity-3 items-start justify-between lg:h-[12hv] mx-auto z-50 xl:items-center opacity-80'>
       <div className='h-full w-10/12 my-auto flex justify-between items-center'>
         <h2 className='text-3xl text-mainColor  font-titleFont font-semibold px-4'>
-          <Link href='/'>Urban Chronicles</Link>
+          <Link href='/'>Time Totes</Link>
         </h2>
         <ul className='flex justify-center items-center text-bgColor gap-6  text-[18px]'>
           <Link href='/section/women' className='cursor-pointer font-titleFont uppercase transition-all hover:text-mainColor'>Women</Link >
